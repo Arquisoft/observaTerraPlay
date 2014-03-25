637199d60b97cea86ef0e077cbdaeed56aca21c3
Labra <jelabra@gmail.com>
2014-03-25 08:01:46 +0100

    Added JSOn generator for countries

---
+++ app/controllers/API.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import java.util.List;
+
+import models.Country;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import com.fasterxml.jackson.databind.node.*;
+
+public class API extends Controller {
+
+    public static Result countries() {
+    	List<Country> countries = Country.all();
+    	JsonNodeFactory factory = JsonNodeFactory.instance;
+        ObjectNode result = new ObjectNode(factory);
+        ArrayNode array = new ArrayNode(factory);
+    	for (Country country : countries) {
+    		ObjectNode obj = Json.newObject();
+        	obj.put("code", country.code);
+        	obj.put("name", country.name);
+        	array.add(obj);
+    	}
+    	result.put("countries",array);
+    	return ok(result);
+    }
+
+}
