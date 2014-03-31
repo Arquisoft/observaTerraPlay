72e494473993ee8178d779c9df637e05d6cb053b
labra <jelabra@gmail.com>
2014-03-31 23:04:26 +0200

    Added simple Ajax example

--- app/controllers/Pruebas.java
+++ app/controllers/Pruebas.java
@@ -5,15 +5,20 @@ import play.libs.F.Promise;
 import play.libs.WS;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.*;
 
-public class Proxy extends Controller {
+public class Pruebas extends Controller {
 
-	public static Promise<Result> index(String url) {
+	public static Promise<Result> proxy(String url) {
 		F.Promise<WS.Response> responsePromise = WS.url(url).get();
 		return responsePromise.map(new F.Function<WS.Response,Result>(){
 			public Result apply(WS.Response response) throws Throwable {
 				return ok(response.getBody()).as("text/html");
 			}
 		});
+	}
+	
+	public static Result ajax() {
+		return ok(ajax.render());
 	}
 }
